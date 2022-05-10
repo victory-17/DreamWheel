e449e671899ff517dc45a699e14319e278dd6f1f
victory-17 <abdo17nasr@gmail.com>
2022-05-10 03:21:32 +0200

    First push

---
+++ battery replacment/js/script.js
@@ -0,0 +1,146 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+// login form
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+document.querySelector('#login-btn').onclick = () => {
+    document.querySelector('.login-form-container').classList.toggle('active');
+}
+
+document.querySelector('#close-login-form').onclick = () => {
+    document.querySelector('.login-form-container').classList.remove('active');
+}
+
+// navbar animation while scrolling
+window.onscroll = () => {
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    };
+
+};
+
+// Theme changer button
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+});
+
+
+// Home page on hover-animation
+document.querySelector('.home').onmousemove = (e) => {
+
+    document.querySelectorAll('.home-parallax').forEach(elm => {
+
+        let speed = elm.getAttribute('data-speed');
+
+        let x = (window.innerWidth - e.pageX * speed) / 90;
+        let y = (window.innerHeight - e.pageY * speed) / 90;
+
+        elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
+
+    });
+
+};
+
+
+document.querySelector('.home').onmouseleave = (e) => {
+
+    document.querySelectorAll('.home-parallax').forEach(elm => {
+
+        elm.style.transform = `translateX(0px) translateY(0px)`;
+
+    });
+
+};
+
+// slider of vehicles section
+var swiper = new Swiper(".vehicles-slider", {
+    grabCursor: true,
+    centeredSlides: true,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+// slider of featured cars section
+var swiper = new Swiper(".featured-slider", {
+    grabCursor: true,
+    centeredSlides: true,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+// slider of team section
+var swiper = new Swiper(".team-slider", {
+    grabCursor: true,
+    centeredSlides: true,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
